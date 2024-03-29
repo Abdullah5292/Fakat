b3ef68a2c985efd9bbf123b3b05acb9e154abd14
Abdullah5292 <112702464+Abdullah5292@users.noreply.github.com>
2024-03-29 15:08:41 +0500

    fixing mailing

--- emailService.js
+++ emailService.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: "contactmanager29@gmail.com",
-        pass: "qffu ialg uwpd nxvy"
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
